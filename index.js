e940d6570d1d30dd208de39f02dfb2bdb48c2a15
Tomas Storc <tomas.storc@gmail.com>
2022-02-12 16:25:30 +0100

    ci change to azure

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
+app.get("/test", (req, res) => {
+  res.send("ci test");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
